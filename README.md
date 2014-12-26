b6a4fc52c4fba2b054a9f7f865cee7a814229661
Fernando Via Canel <fernando.canel@klarna.com>
2014-12-26 14:07:36 +0100

    Correct

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-sidney
+sydney
 ======
---
+++ sydney.js
@@ -0,0 +1,12 @@
+var example   = require("washington")
+var assert    = require("assert")
+
+example("Async: by default")
+
+example("Endpoint: match with glob style wildcards")
+
+example("Repeater: Repeat events back and forth on repeater venue")
+
+example("Repeater: Scope out prefix when sending to repeater")
+
+example("Repeater: Add prefix when receiving from repeater")
