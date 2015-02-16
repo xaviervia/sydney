972d0429ca2bc912757e7f655fa0abf150761c8d
Xavier Via <fernando.via@gmail.com>
2015-02-17 00:21:23 +0100

    Prepare next examples

---
+++ examples.js
@@ -0,0 +1,123 @@
+var example = require("washington")
+var Sydney  = require("./sydney")
+
+
+
+example("notify: call async by default", function (done) {
+  var value   = false
+  var venue  = new Sydney(
+    { match: function () { return true } },
+    function () { value = this.value },
+    { value: true }
+  )
+  venue.notify()
+
+  if (value)
+    throw new Error("Should be async")
+
+  process.nextTick(function () {
+    done( value ? null : new Error("Should be run by now") )
+  })
+})
+
+
+
+example("notify: call with setImmediate if process.nextTick is not available", function (done) {
+  var value   = false
+  var venue  = new Sydney(
+    { match: function () { return true } },
+    function () { value = this.value },
+    { value: true }
+  )
+  var nextTick = process.nextTick
+  process.nextTick = null
+
+  venue.notify()
+
+  process.nextTick = nextTick
+
+  if (value)
+    throw new Error("Should be async")
+
+  process.nextTick(function () {
+    done( value ? new Error("Should not be run yet") : null )
+  })
+
+  setImmediate(function () {
+    done( value ? null : new Error("Should be run by now") )
+  })
+})
+
+
+
+example("notify: call with setTimeout if setImmediate is not available", function (done) {
+  var value   = false
+  var venue  = new Sydney(
+    { match: function () { return true } },
+    function () { value = this.value },
+    { value: true }
+  )
+  var nextTick = process.nextTick
+  var setImmediate = global.setImmediate
+  process.nextTick = null
+  global.setImmediate = null
+
+  venue.notify()
+
+  process.nextTick = nextTick
+  global.setImmediate = setImmediate
+
+  if (value)
+    throw new Error("Should be async")
+
+  process.nextTick(function () {
+    done( value ? new Error("Should not be run yet") : null )
+  })
+
+  setImmediate(function () {
+    done( value ? new Error("Should not be run yet") : null )
+  })
+
+  setTimeout(function () {
+    done( value ? null : new Error("Should have been run by now") )
+  }, 0)
+})
+
+
+example("notify: should not run if it does not match", function (done) {
+  var value = false
+  var venue = new Sydney(
+    { match: function () { return false } },
+    function () { value = true },
+    { context: "something" }
+  )
+
+  venue.notify()
+
+  process.nextTick(function () {
+    done( !value ? null : new Error("Shouldn't have run") )
+  })
+})
+
+
+example("notify: forwards the event")
+
+
+example("nofify: calls with the proper context")
+
+
+example("match: delegates to the endpoint match", function () {
+  var result = {}
+  var endpoint = {
+    match: function () {
+      this.match.args = arguments
+      return result } }
+  var event = { a: 'particular event' }
+  var venue = new Sydney( endpoint, function () {}, {})
+
+  if (venue.match(event) !== result)
+    throw new Error("Should have returned the result of endpoint match")
+
+  if (endpoint.match.args[0] !== event)
+    throw new Error("Should have forwarded the event object")
+})
--- sydney.js
+++ sydney.js
@@ -1,30 +1,12 @@
-var example   = require("washington")
-var assert    = require("assert")
-
-/*
-
-If the callback is by default a filter over the execution of the contained subscriptions... we should I
-
-*/
-//
-//
-// new Sydney(function (event, venue) {
-//   venue.run(event)
-// }, { name: "context" })
-//
-//
-// Sydney({
-//   match: function (event, venue) {
-//     venue.run(event)
-//   }
-// }, function () {}, { name: "context" })
 var Sydney = function (endpoint, callback, context) {
   this.endpoint = endpoint
   this.callback = callback
   this.context  = context
 }
 
-Sydney.prototype.run = function () {
+Sydney.prototype.notify = function () {
+  if (!this.match()) return
+
   if (process && process.nextTick)
     process.nextTick(function () {
       this.callback.call(this.context)
@@ -39,78 +21,8 @@ Sydney.prototype.run = function () {
     }.bind(this))
 }
 
-example("Call async by default", function (done) {
-  var value   = false
-  var sydney  = new Sydney(
-    { match: function () { return true } },
-    function () { value = this.value },
-    { value: true }
-  )
-  sydney.run()
+Sydney.prototype.match = function (event) {
+  return this.endpoint.match(event)
+}
 
-  if (value)
-    throw new Error("Should be async")
-
-  process.nextTick(function () {
-    done( value ? null : new Error("Should be run by now") )
-  })
-})
-
-example("Call with setImmediate if process.nextTick is not available", function (done) {
-  var value   = false
-  var sydney  = new Sydney(
-    { match: function () { return true } },
-    function () { value = this.value },
-    { value: true }
-  )
-  var nextTick = process.nextTick
-  process.nextTick = null
-
-  sydney.run()
-
-  process.nextTick = nextTick
-
-  if (value)
-    throw new Error("Should be async")
-
-  process.nextTick(function () {
-    done( value ? new Error("Should not be run yet") : null )
-  })
-
-  setImmediate(function () {
-    done( value ? null : new Error("Should be run by now") )
-  })
-})
-
-example("Call with setTimeout if setImmediate is not available", function (done) {
-  var value   = false
-  var sydney  = new Sydney(
-    { match: function () { return true } },
-    function () { value = this.value },
-    { value: true }
-  )
-  var nextTick = process.nextTick
-  var setImmediate = global.setImmediate
-  process.nextTick = null
-  global.setImmediate = null
-  
-  sydney.run()
-
-  process.nextTick = nextTick
-  global.setImmediate = setImmediate
-
-  if (value)
-    throw new Error("Should be async")
-
-  process.nextTick(function () {
-    done( value ? new Error("Should not be run yet") : null )
-  })
-
-  setImmediate(function () {
-    done( value ? new Error("Should not be run yet") : null )
-  })
-
-  setTimeout(function () {
-    done( value ? null : new Error("Should have been run by now") )
-  }, 0)
-})
+module.exports = Sydney
