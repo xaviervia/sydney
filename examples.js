5437aca6410d3b406512f21e445ccba08e9e4dc8
Xavier Via <fernando.via@gmail.com>
2015-02-19 22:48:49 +0100

    Adapt to new Washington passive assertions

--- examples.js
+++ examples.js
@@ -3,7 +3,7 @@ var Sydney  = require("./sydney")
 
 
 
-example("notify: call async by default", function (done) {
+example("notify: call async by default", function (check) {
   var value   = false
   var venue  = new Sydney(
     { match: function () { return true } },
@@ -12,17 +12,16 @@ example("notify: call async by default", function (done) {
   )
   venue.notify()
 
-  if (value)
-    throw new Error("Should be async")
+  if (value) check(Error("Should be async"))
 
   process.nextTick(function () {
-    done( value ? undefined : Error("Should be run by now") )
+    check( value || Error("Should be run by now") )
   })
 })
 
 
 
-example("notify: call with setImmediate if process.nextTick is not available", function (done) {
+example("notify: call with setImmediate if process.nextTick is not available", function (check) {
   var value   = false
   var venue  = new Sydney(
     { match: function () { return true } },
@@ -36,21 +35,20 @@ example("notify: call with setImmediate if process.nextTick is not available", f
 
   process.nextTick = nextTick
 
-  if (value)
-    throw new Error("Should be async")
+  if (value) check(Error("Should be async"))
 
   process.nextTick(function () {
-    done( value ? Error("Should not be run yet") : undefined )
+    check( !value || Error("Should not be run yet") )
   })
 
   setImmediate(function () {
-    done( value ? undefined : Error("Should be run by now") )
+    check( value || Error("Should be run by now") )
   })
 })
 
 
 
-example("notify: call with setTimeout if setImmediate is not available", function (done) {
+example("notify: call with setTimeout if setImmediate is not available", function (check) {
   var value           = false
   var venue           = new Sydney(
     { match: function () { return true } },
@@ -67,24 +65,23 @@ example("notify: call with setTimeout if setImmediate is not available", functio
   process.nextTick = nextTick
   global.setImmediate = setImmediate
 
-  if (value)
-    throw Error("Should be async")
+  if (value) check(Error("Should be async"))
 
   process.nextTick(function () {
-    done( value ? Error("Should not be run yet") : undefined )
+    check( !value || Error("Should not be run yet") )
   })
 
   setImmediate(function () {
-    done( value ? Error("Should not be run yet") : undefined )
+    check( !value || Error("Should not be run yet") )
   })
 
   setTimeout(function () {
-    done( value ? undefined : Error("Should have been run by now") )
+    check( value || Error("Should have been run by now") )
   }, 0)
 })
 
 
-example("notify: should not run if it does not match", function (done) {
+example("notify: should not run if it does not match", function (check) {
   var value = false
   var venue = new Sydney(
     { match: function () { return false } },
@@ -95,12 +92,12 @@ example("notify: should not run if it does not match", function (done) {
   venue.notify()
 
   process.nextTick(function () {
-    done( !value ? undefined : Error("Shouldn't have run") )
+    check( !value || Error("Shouldn't have run") )
   })
 })
 
 
-example("notify: forwards the event", function (done) {
+example("notify: forwards the event", function (check) {
   var value     = false
   var someEvent = { content: "text" }
   var venue     = new Sydney(
@@ -112,13 +109,12 @@ example("notify: forwards the event", function (done) {
   venue.notify(someEvent)
 
   process.nextTick(function () {
-    done( value === someEvent ?
-      undefined : Error("Should have been the event") )
+    check( value === someEvent || Error("Should have been the event") )
   })
 })
 
 
-example("notify: calls with the proper context", function (done) {
+example("notify: calls with the proper context", function (check) {
   var value     = undefined
   var someEvent = { content: "different" }
   var context   = { name: { last: "Bond", first: "James" } }
@@ -131,8 +127,9 @@ example("notify: calls with the proper context", function (done) {
   venue.notify(someEvent)
 
   process.nextTick(function () {
-    done( value === context.name ?
-      undefined : Error("Should have been the context.name") )
+    check(
+      value === context.name ||
+      Error("Should have been the context.name") )
   })
 })
 
@@ -147,8 +144,8 @@ example("match: delegates to the endpoint match", function () {
   var venue = new Sydney( endpoint, function () {}, {})
 
   if (venue.match(event) !== result)
-    throw new Error("Should have returned the result of endpoint match")
+    return "Should have returned the result of endpoint match"
 
   if (endpoint.match.args[0] !== event)
-    throw new Error("Should have forwarded the event object")
+    return "Should have forwarded the event object"
 })
