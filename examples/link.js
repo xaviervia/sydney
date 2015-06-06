28472cb7cc80bdb277d3af60c5077fe1171c9e5e
Xavier Via <fernando.via@gmail.com>
2015-06-06 21:53:10 +0200

    Update #link examples to simplified interface

--- examples/link.js
+++ examples/link.js
@@ -26,56 +26,227 @@ example("#link: subscribe back", function () {
 })
 
 
-example("#link: @function create venue and add as subscriber", function () {
+example("#link: add @vanilla module as subscriber", function () {
+  var mother    = new Sydney
+  var daughter  = {}
+
+  mother.link(daughter)
+
+  return mother.subscribers[0] === daughter
+})
+
+
+example("#link: @vanilla add venue back", function () {
+  var mother    = new Sydney
+  var daughter  = {}
+
+  mother.link(daughter)
+
+  return daughter.subscribers[0] === mother
+})
+
+
+example("#link: amplify @vanilla module with #add", function () {
+  var venue       = new Sydney
+  var subscriber  = {}
+
+  venue.link(subscriber)
+
+  return subscriber.add === Sydney.prototype.add
+})
+
+
+example("#link: amplify @vanilla module with #broadcast", function () {
+  var venue       = new Sydney
+  var subscriber  = {}
+
+  venue.link(subscriber)
+
+  return subscriber.broadcast === Sydney.prototype.broadcast
+})
+
+
+example("#link: amplify @vanilla module with #link", function () {
+  var venue       = new Sydney
+  var subscriber  = {}
+
+  venue.link(subscriber)
+
+  return subscriber.link === Sydney.prototype.link
+})
+
+
+example("#link: amplify @vanilla module with #remove", function () {
+  var venue       = new Sydney
+  var subscriber  = {}
+
+  venue.link(subscriber)
+
+  return subscriber.remove === Sydney.prototype.remove
+})
+
+
+example("#link: amplify @vanilla module with #send", function () {
+  var venue       = new Sydney
+  var subscriber  = {}
+
+  venue.link(subscriber)
+
+  return subscriber.send === Sydney.prototype.send
+})
+
+
+example("#link: amplify @vanilla module with #unlink", function () {
+  var venue       = new Sydney
+  var subscriber  = {}
+
+  venue.link(subscriber)
+
+  return subscriber.unlink === Sydney.prototype.unlink
+})
+
+
+example("#link: don't replace @vanilla #add property", function () {
+  var venue       = new Sydney
+  var property    = { name: "original" }
+  var subscriber  = {
+    add: property
+  }
+
+  venue.link(subscriber)
+
+  return subscriber.add === property
+})
+
+
+example("#link: don't replace @vanilla #broadcast property", function () {
+  var venue       = new Sydney
+  var property    = { name: "original" }
+  var subscriber  = {
+    broadcast: property
+  }
+
+  venue.link(subscriber)
+
+  return subscriber.broadcast === property
+})
+
+
+example("#link: don't replace @vanilla #link property", function () {
+  var venue       = new Sydney
+  var property    = { name: "original" }
+  var subscriber  = {
+    link: property
+  }
+
+  venue.link(subscriber)
+
+  return subscriber.link === property
+})
+
+
+example("#link: don't replace @vanilla #remove property", function () {
+  var venue       = new Sydney
+  var property    = { name: "original" }
+  var subscriber  = {
+    remove: property
+  }
+
+  venue.link(subscriber)
+
+  return subscriber.remove === property
+})
+
+
+example("#link: don't replace @vanilla #send property", function () {
+  var venue       = new Sydney
+  var property    = { name: "original" }
+  var subscriber  = {
+    send: property
+  }
+
+  venue.link(subscriber)
+
+  return subscriber.send === property
+})
+
+
+example("#link: don't replace @vanilla #unlink property", function () {
+  var venue       = new Sydney
+  var property    = { name: "original" }
+  var subscriber  = {
+    unlink: property
+  }
+
+  venue.link(subscriber)
+
+  return subscriber.unlink === property
+})
+
+
+example("#link: don't replace @vanilla #subscribers property", function () {
+  var venue       = new Sydney
+  var subscribers = []
+  var subscriber  = {
+    subscribers: subscribers
+  }
+
+  venue.link(subscriber)
+
+  return subscriber.subscribers === subscribers
+})
+
+
+example("#link: @regression @function doesn't add as subscriber", function () {
   var mother            = new Sydney
   var daughterCallback  = function () {}
 
   mother.link(daughterCallback)
 
-  return mother.subscribers[0].callback === daughterCallback
+  return mother.subscribers[0].callback !== daughterCallback
 })
 
 
-example("#link: @function subscribe back", function () {
+example("#link: @regression @function doesn't subscribe back", function () {
   var mother            = new Sydney
   var daughterCallback  = function () {}
 
   mother.link(daughterCallback)
 
-  return mother.subscribers[0].subscribers[0] === mother
+  return mother.subscribers[0].subscribers[0] !== mother
 })
 
 
-example("#link: @endpoint create venue and add endpoint", function () {
+example("#link: @regression @endpoint doesn't add endpoint", function () {
   var mother            = new Sydney
   var endpoint          = { match: function () {} }
   var daughterCallback  = function () {}
 
   mother.link(endpoint, daughterCallback)
 
-  return mother.subscribers[0].endpoint === endpoint
+  return mother.subscribers[0].endpoint !== endpoint
 })
 
 
-example("#link: @endpoint create venue and add callback", function () {
+example("#link: @regression @endpoint doesn't add callback", function () {
   var mother            = new Sydney
   var endpoint          = { match: function () {} }
   var callback          = function () {}
 
   mother.link(endpoint, callback)
 
-  return mother.subscribers[0].callback === callback
+  return mother.subscribers[0].callback !== callback
 })
 
 
-example("#link: @endpoint subscribe back", function () {
+example("#link: @regression @endpoint doesn't subscribe back", function () {
   var mother            = new Sydney
   var endpoint          = { match: function () {} }
   var callback          = function () {}
 
   mother.link(endpoint, callback)
 
-  return mother.subscribers[0].subscribers[0] === mother
+  return mother.subscribers[0].subscribers[0] !== mother
 })
 
 
@@ -83,110 +254,4 @@ example("#link: chainable", function () {
   var mother = new Sydney
 
   return mother.link(function () {}) === mother
-})
-
-
-example("#link: @vanilla @callback the Sydney is added back", function () {
-  var venue       = new Sydney
-  var subscriber  = {
-    callback: function () {
-      return this === subscriber
-    }
-  }
-
-  venue.link(subscriber)
-
-  return venue.subscribers[0].subscribers[0] === venue
-})
-
-
-example("#link: @vanilla @callback a Sydney is created", function () {
-  var venue       = new Sydney
-  var subscriber  = {
-    callback: function () {
-      return this === subscriber
-    }
-  }
-
-  venue.link(subscriber)
-
-  return venue.subscribers[0] instanceof Sydney
-})
-
-
-example("#link: @vanilla @callback is bound to original context", function () {
-  var venue       = new Sydney
-  var subscriber  = {
-    callback: function () {
-      return this === subscriber
-    }
-  }
-
-  venue.link(subscriber)
-
-  return venue.subscribers[0].callback()
-})
-
-
-example("#link: @vanilla @endpoint a Sydney is created", function () {
-  var venue       = new Sydney
-  var subscriber  = {
-    endpoint: {
-      match: function () {}
-    }
-  }
-
-  venue.link(subscriber)
-
-  return venue.subscribers[0] instanceof Sydney
-})
-
-
-example("#link: @vanilla @endpoint is preserved", function () {
-  var venue       = new Sydney
-  var subscriber  = {
-    endpoint: {
-      match: function () {}
-    }
-  }
-
-  venue.link(subscriber)
-
-  return venue.subscribers[0].endpoint === subscriber.endpoint
-})
-
-
-example("#link: @vanilla @endpoint & @callback, callback keeps context", function () {
-  var venue       = new Sydney
-  var subscriber  = {
-    callback: function () {
-      return this === subscriber
-    },
-
-    endpoint: {
-      match: function () {}
-    }
-  }
-
-  venue.link(subscriber)
-
-  return venue.subscribers[0].callback()
-})
-
-
-example("#link: @vanilla @endpoint & @callback, endpoint is kept", function () {
-  var venue       = new Sydney
-  var subscriber  = {
-    callback: function () {
-      return this === subscriber
-    },
-
-    endpoint: {
-      match: function () {}
-    }
-  }
-
-  venue.link(subscriber)
-
-  return venue.subscribers[0].endpoint === subscriber.endpoint
 })
